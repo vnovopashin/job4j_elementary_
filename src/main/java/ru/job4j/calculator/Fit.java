571698b256ae5ed8c88cb9e8769b394f1e369253
nova <novopashinva@gmail.com>
2026-01-11 17:33:02 +0700

    6. Идеальный вес [#41584]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,21 @@
+package ru.job4j.calculator;
+
+public class Fit {
+    public static double manWeight(short height) {
+        double result = (height - 100) * 1.15;
+        return result;
+    }
+
+    public static double womanWeight(short height) {
+        double result = (height - 110) * 1.15;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = Fit.manWeight(height);
+        System.out.println("Man 187 is " + man);
+        double woman = Fit.womanWeight(height);
+        System.out.println("Woman 187 is " + woman);
+    }
+}
