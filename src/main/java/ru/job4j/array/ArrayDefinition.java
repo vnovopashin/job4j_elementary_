8bdc5b649f2ab52e4ee59fdd845a00ecd61843c0
nova <novopashinva@gmail.com>
2026-02-05 22:42:11 +0700

    6.0.3. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,6 +5,12 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Ivan";
+        names[1] = "Sergey";
+        names[2] = "Alex";
+        names[3] = "Irina";
+        System.out.printf("%s\n%s\n%s\n%s\n\n", names[0], names[1], names[2], names[3]);
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
